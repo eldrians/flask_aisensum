cd7fed363033e40a6d3174aba42f0fb82b7daa03
axel eldrian hadiwibowo <eldrianaxel1@gmail.com>
2024-02-13 21:34:26 +0700

    api(): fix import file

--- api/index.py
+++ api/index.py
@@ -1,13 +1,9 @@
 from flask import Flask
-from model.customer_model import customer_model
 
 app = Flask(__name__)
-obj = customer_model()
 
 @app.route("/")
 def welcome():
     return "Axel Eldrian Hadiwibowo v2"
 
-@app.route("/customer")
-def customer_get_all_controller():
-    return obj.customer_get_all_model()
+from controller import *
