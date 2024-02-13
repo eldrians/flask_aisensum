387caee03b4cf079e93c65198dbbefd2770717da
axel eldrian hadiwibowo <eldrianaxel1@gmail.com>
2024-02-13 21:13:53 +0700

    app(): check first try

--- api/index.py
+++ api/index.py
@@ -1,9 +1,13 @@
 from flask import Flask
+from model.customer_model import customer_model
 
 app = Flask(__name__)
+obj = customer_model()
 
 @app.route("/")
 def welcome():
     return "Axel Eldrian Hadiwibowo v2"
 
-# from controller import *
+@app.route("/customer")
+def customer_get_all_controller():
+    return obj.customer_get_all_model()
