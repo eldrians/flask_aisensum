43d7cecd540eb58984a024ac1afd909892e987d5
axel eldrian hadiwibowo <eldrianaxel1@gmail.com>
2024-02-13 21:09:46 +0700

    api(): check error

--- api/index.py
+++ api/index.py
@@ -4,6 +4,6 @@ app = Flask(__name__)
 
 @app.route("/")
 def welcome():
-    return "Axel Eldrian Hadiwibowo"
+    return "Axel Eldrian Hadiwibowo v2"
 
-from controller import *
+# from controller import *
